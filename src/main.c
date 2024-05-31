1ab5560caa2ca03a9ced2a8c6a1d3b9e89d8af1f
luhong <cmanlh@163.com>
2024-05-31 07:01:50 +0000

    add poc code

--- src/main.c
+++ src/main.c
@@ -72,3 +72,74 @@ int main(int argc, char const *argv[])
     // test_utf8CodePoint();
     // test_utf8Char();
 }
+
+void readJson()
+{
+    const char *json = "{\"name\":\"Mash中国\",\"star\":4,\"hits\":[2,2,1,3]}";
+
+    // Read JSON and get root
+    yyjson_doc *doc = yyjson_read(json, strlen(json), 0);
+    yyjson_val *root = yyjson_doc_get_root(doc);
+
+    // Get root["name"]
+    yyjson_val *name = yyjson_obj_get(root, "name");
+    printf("name: %s\n", yyjson_get_str(name));
+    printf("name length:%d\n", (int)yyjson_get_len(name));
+
+    // Get root["star"]
+    yyjson_val *star = yyjson_obj_get(root, "star");
+    printf("star: %d\n", (int)yyjson_get_int(star));
+
+    // Get root["hits"], iterate over the array
+    yyjson_val *hits = yyjson_obj_get(root, "hits");
+    size_t idx, max;
+    yyjson_val *hit;
+    yyjson_arr_foreach(hits, idx, max, hit)
+    {
+        printf("hit%d: %d\n", (int)idx, (int)yyjson_get_int(hit));
+    }
+
+    // Free the doc
+    yyjson_doc_free(doc);
+}
+
+void hash()
+{
+    wchar_t *path = L"C:\\codespace\\chinese-poetry\\全唐诗\\poet.tang.0.json";
+    struct stat fileStat = {};
+    wstat(path, &fileStat);
+
+    FILE *fp = _wfopen(path, L"r");
+    char json[fileStat.st_size + 1];
+    int jsonCounter = 0;
+    char buff[1024];
+    while (fgets(buff, 1024, fp) != NULL)
+    {
+        int i = 0;
+        while (true)
+        {
+            if ('\0' == buff[i])
+            {
+                break;
+            }
+
+            json[jsonCounter++] = buff[i++];
+        }
+    }
+    fclose(fp);
+    json[jsonCounter] = '\0';
+
+    yyjson_doc *doc = yyjson_read(json, strlen(json), 0);
+    yyjson_val *root = yyjson_doc_get_root(doc);
+    size_t idx, max;
+    yyjson_val *hit;
+    // yyjson_arr_foreach(root, idx, max, hit)
+    // {
+    //     printf("%d/%d: %s\n", (int)idx, (int)max, yyjson_get_str(yyjson_obj_get(hit, "author")));
+    // }
+
+    uint32_t num = 12323;
+    printf("%d\n", XXH32(&num, sizeof(num), 32));
+    printf("%d\n", XXH32(&num, sizeof(num), 64));
+    printf("%d\n", XXH32(&num, sizeof(num), 32));
+}
